4b8e2a2f5c8d9ecefed81d473ebc36c0ca0d2ab7
cem-ergin <cem-ergin@outlook.com>
2020-07-12 00:04:06 +0300

    guzel bir ilerleyis

--- lib/pages/note_page/note_page.dart
+++ lib/pages/note_page/note_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:gunluk/constants/my_themes.dart';
 import 'package:gunluk/pages/note_page/ui/gallery_camera_view.dart';
 import 'package:gunluk/shared/date_helper.dart';
 import 'package:slider_button/slider_button.dart';
@@ -12,11 +13,11 @@ class NotePage extends StatefulWidget {
 }
 
 class _NotePageState extends State<NotePage> {
-  TextEditingController myTextController;
+  final TextEditingController _myTextController = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
   @override
   void initState() {
     super.initState();
-    myTextController = TextEditingController();
   }
 
   @override
@@ -28,9 +29,7 @@ class _NotePageState extends State<NotePage> {
           appBar: AppBar(
             elevation: 100,
             leading: GestureDetector(
-              onTap: () {
-                Navigator.pop(context);
-              },
+              onTap: () => Navigator.pop(context),
               child: Icon(Icons.arrow_back_ios),
             ),
             backgroundColor: Theme.of(context).primaryColor,
@@ -39,35 +38,35 @@ class _NotePageState extends State<NotePage> {
           body: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                    '${DateHelper().getMMMMd(DateTime.now())} | ${myTextController.text.length} karakter'),
-              ),
+              buildDateAndLengthView(),
               Expanded(
-                child: Container(
-                  child: Scrollbar(
-                    child: SingleChildScrollView(
-                      scrollDirection: Axis.vertical,
-                      //reverse: true,
-                      child: Padding(
-                        padding: const EdgeInsets.only(
-                            left: 8.0, right: 8.0, top: 4.0),
-                        child: TextField(
-                          controller: myTextController,
-                          onChanged: onTextChanged,
-                          keyboardType: TextInputType.multiline,
-                          textAlign: TextAlign.justify,
-                          maxLines: null,
-                          decoration: InputDecoration(
-                            hintText: 'Yazmaya baslayabilirsin',
-                            hintStyle: TextStyle(color: Colors.grey),
-                            border: InputBorder.none,
-                            focusedBorder: InputBorder.none,
-                            disabledBorder: InputBorder.none,
-                            enabledBorder: InputBorder.none,
-                            errorBorder: InputBorder.none,
-                            focusedErrorBorder: InputBorder.none,
+                child: GestureDetector(
+                  onTap: () => _focusNode.requestFocus(),
+                  child: Container(
+                    child: Scrollbar(
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.vertical,
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                              left: 8.0, right: 8.0, top: 4.0),
+                          child: TextField(
+                            focusNode: _focusNode,
+                            controller: _myTextController,
+                            onChanged: onTextChanged,
+                            textCapitalization: TextCapitalization.sentences,
+                            keyboardType: TextInputType.multiline,
+                            textAlign: TextAlign.justify,
+                            maxLines: null,
+                            decoration: InputDecoration(
+                              hintText: 'Yazmaya baslayabilirsin',
+                              hintStyle: TextStyle(color: Colors.grey),
+                              border: InputBorder.none,
+                              focusedBorder: InputBorder.none,
+                              disabledBorder: InputBorder.none,
+                              enabledBorder: InputBorder.none,
+                              errorBorder: InputBorder.none,
+                              focusedErrorBorder: InputBorder.none,
+                            ),
                           ),
                         ),
                       ),
@@ -75,85 +74,142 @@ class _NotePageState extends State<NotePage> {
                   ),
                 ),
               ),
-              Row(
+              buildBottomSection()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Padding buildDateAndLengthView() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text(
+          '${DateHelper().getMMMMd(DateTime.now())} | ${_myTextController.text.length} karakter'),
+    );
+  }
+
+  Row buildBottomSection() {
+    return Row(
+      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        buildGalleryCameraButton(),
+        buildTextFieldButton(),
+        buildThemeButton(),
+        buildRecordButton(),
+        buildMoreButton(),
+      ],
+    );
+  }
+
+  Expanded buildRecordButton() {
+    return Expanded(
+      child: IconButton(
+        icon: FaIcon(FontAwesomeIcons.voicemail),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Expanded buildThemeButton() {
+    return Expanded(
+      child: IconButton(
+        icon: FaIcon(FontAwesomeIcons.tshirt),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Expanded buildTextFieldButton() {
+    return Expanded(
+      child: IconButton(
+        icon: Icon(Icons.text_fields),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Expanded buildGalleryCameraButton() {
+    return Expanded(
+      child: GalleryCameraViewNotePage(),
+    );
+  }
+
+  Expanded buildMoreButton() {
+    return Expanded(
+      child: IconButton(
+        icon: Icon(Icons.more_vert),
+        onPressed: _onMoreButtonPressed,
+      ),
+    );
+  }
+
+  void _onMoreButtonPressed() {
+    final _myThemes = MyThemes.myThemes();
+    showModalBottomSheet(
+      context: context,
+      builder: (_) {
+        return Container(
+          color: Theme.of(context).primaryColor,
+          child: SafeArea(
+            child: Container(
+              color: Theme.of(context).scaffoldBackgroundColor,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Expanded(
-                    child: GalleryCameraViewNotePage(),
+                  FlatButton.icon(
+                    icon: Icon(Icons.delete),
+                    label: Text("Delete"),
+                    onPressed: () {},
                   ),
-                  Expanded(
-                    child: IconButton(
-                      icon: Icon(Icons.text_fields),
-                      onPressed: () {},
-                    ),
+                  FlatButton.icon(
+                    icon: Icon(Icons.share),
+                    label: Text("Share"),
+                    onPressed: () {},
                   ),
-                  Expanded(
-                    child: IconButton(
-                      icon: FaIcon(FontAwesomeIcons.tshirt),
-                      onPressed: () {},
-                    ),
-                  ),
-                  Expanded(
-                    child: IconButton(
-                      icon: FaIcon(FontAwesomeIcons.voicemail),
-                      onPressed: () {
-                        showGeneralDialog(
-                          barrierLabel: "Label",
-                          barrierDismissible: true,
-                          barrierColor: Colors.black.withOpacity(0.5),
-                          transitionDuration: Duration(milliseconds: 350),
-                          context: context,
-                          pageBuilder: (context, anim1, anim2) {
-                            return Align(
-                              alignment: Alignment.bottomRight,
-                              child: Container(
-                                child: SliderButton(
-                                  vibrationFlag: true,
-                                  action: () {
-                                    Navigator.pop(context);
-                                  },
-                                  label: Text(
-                                    "Ses kaydi icin kaydir",
-                                    style: TextStyle(
-                                      color: Color(0xff4a4a4a),
-                                      //fontWeight: FontWeight.w500,
-                                      fontSize: 14,
-                                    ),
-                                  ),
-                                  icon: Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Icon(Icons.fiber_smart_record),
-                                  ),
-                                ),
-                                margin: EdgeInsets.only(
-                                    bottom: 50, left: 12, right: 12),
-                                decoration: BoxDecoration(
-                                  color: Theme.of(context)
-                                      .primaryColor
-                                      .withOpacity(0.9),
-                                  //color: Colors.white,
-                                  borderRadius: BorderRadius.circular(40),
-                                ),
-                              ),
-                            );
-                          },
-                          transitionBuilder: (context, anim1, anim2, child) {
-                            return SlideTransition(
-                              position:
-                                  Tween(begin: Offset(1, 1), end: Offset(0, 0))
-                                      .animate(anim1),
-                              child: child,
-                            );
-                          },
+                  // FlatButton.icon(
+                  //   icon: Icon(Icons.font_download),
+                  //   label: Text("Change font"),
+                  //   onPressed: () {},
+                  // ),
+                  // FlatButton.icon(
+                  //   icon: Icon(Icons.photo),
+                  //   label: Text("Add photo"),
+                  //   onPressed: () {},
+                  // ),
+                  // FlatButton.icon(
+                  //   icon: Icon(Icons.keyboard_voice),
+                  //   label: Text("Record voice"),
+                  //   onPressed: () {},
+                  // ),
+                  Container(
+                    height: 40,
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: _myThemes.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Padding(
+                          padding: const EdgeInsets.only(
+                            top: 2.0,
+                            bottom: 8.0,
+                          ),
+                          child: CircleAvatar(
+                            radius: 24,
+                            backgroundColor: _myThemes[index].primaryColor,
+                          ),
                         );
                       },
                     ),
                   ),
                 ],
-              )
-            ],
+              ),
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 
--- lib/pages/note_page/ui/gallery_camera_view.dart
+++ lib/pages/note_page/ui/gallery_camera_view.dart
@@ -9,69 +9,69 @@ class GalleryCameraViewNotePage extends StatelessWidget {
     return IconButton(
       icon: Icon(Icons.photo_album),
       onPressed: () {
-        showGeneralDialog(
-          barrierLabel: "Label",
-          barrierDismissible: true,
-          barrierColor: Colors.black.withOpacity(0.5),
-          transitionDuration: Duration(milliseconds: 350),
-          context: context,
-          pageBuilder: (context, anim1, anim2) {
-            return Align(
-              alignment: Alignment.bottomLeft,
-              child: Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Flexible(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: OutlineButton(
-                          textColor: Theme.of(context).canvasColor,
-                          child: Text("Gallery"),
-                          onPressed: () {
-                            onGalleryPressed(context);
-                          },
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0),
-                          ),
-                        ),
+        onPhotoAlbumPressed(context);
+      },
+    );
+  }
+
+  void onPhotoAlbumPressed(BuildContext context) {
+    showGeneralDialog(
+      barrierLabel: "Label",
+      barrierDismissible: true,
+      barrierColor: Colors.black.withOpacity(0.5),
+      transitionDuration: Duration(milliseconds: 350),
+      context: context,
+      pageBuilder: (context, anim1, anim2) {
+        return Align(
+          alignment: Alignment.bottomLeft,
+          child: Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Flexible(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: OutlineButton(
+                      textColor: Theme.of(context).canvasColor,
+                      child: Text("Gallery"),
+                      onPressed: () => onGalleryPressed(context),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
                       ),
                     ),
-                    Flexible(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: OutlineButton(
-                          textColor: Theme.of(context).canvasColor,
-                          child: Text("Camera"),
-                          onPressed: () {
-                            onCameraPressed(context);
-                          },
-                          shape: RoundedRectangleBorder(
-                            side: BorderSide(color: Colors.white),
-                            borderRadius: BorderRadius.circular(30.0),
-                          ),
-                        ),
+                  ),
+                ),
+                Flexible(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: OutlineButton(
+                      textColor: Theme.of(context).canvasColor,
+                      child: Text("Camera"),
+                      onPressed: () => onCameraPressed(context),
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.white),
+                        borderRadius: BorderRadius.circular(30.0),
                       ),
                     ),
-                  ],
+                  ),
                 ),
-                margin: EdgeInsets.only(bottom: 50, left: 12, right: 12),
-                decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor.withOpacity(0.9),
-                  //color: Colors.white,
-                  borderRadius: BorderRadius.circular(40),
-                ),
-              ),
-            );
-          },
-          transitionBuilder: (context, anim1, anim2, child) {
-            return SlideTransition(
-              position:
-                  Tween(begin: Offset(-1, 1), end: Offset(0, 0)).animate(anim1),
-              child: child,
-            );
-          },
+              ],
+            ),
+            margin: EdgeInsets.only(bottom: 50, left: 12, right: 12),
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor.withOpacity(0.9),
+              //color: Colors.white,
+              borderRadius: BorderRadius.circular(40),
+            ),
+          ),
+        );
+      },
+      transitionBuilder: (context, anim1, anim2, child) {
+        return SlideTransition(
+          position:
+              Tween(begin: Offset(-1, 1), end: Offset(0, 0)).animate(anim1),
+          child: child,
         );
       },
     );
@@ -83,7 +83,7 @@ class GalleryCameraViewNotePage extends StatelessWidget {
       print(image.path);
       Navigator.pop(context);
     } catch (e) {
-      print("error: " + e.toString());
+      print("onGalleryPressed error: " + e.toString());
     }
   }
 
