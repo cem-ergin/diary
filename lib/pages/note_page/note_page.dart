84dda636e4fd84d2b142a8213384ba85180a5525
cem-ergin <cem-ergin@outlook.com>
2020-07-12 14:55:26 +0300

    pc cok kasiyor

--- lib/pages/note_page/note_page.dart
+++ lib/pages/note_page/note_page.dart
@@ -3,7 +3,6 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:gunluk/constants/my_themes.dart';
 import 'package:gunluk/pages/note_page/ui/gallery_camera_view.dart';
 import 'package:gunluk/shared/date_helper.dart';
-import 'package:slider_button/slider_button.dart';
 
 class NotePage extends StatefulWidget {
   NotePage({Key key}) : super(key: key);
@@ -33,49 +32,58 @@ class _NotePageState extends State<NotePage> {
               child: Icon(Icons.arrow_back_ios),
             ),
             backgroundColor: Theme.of(context).primaryColor,
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.check),
+                onPressed: () {},
+              ),
+            ],
             //title: Text('Title'),
           ),
           body: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               buildDateAndLengthView(),
-              Expanded(
-                child: GestureDetector(
-                  onTap: () => _focusNode.requestFocus(),
-                  child: Container(
-                    child: Scrollbar(
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.vertical,
-                        child: Padding(
-                          padding: const EdgeInsets.only(
-                              left: 8.0, right: 8.0, top: 4.0),
-                          child: TextField(
-                            focusNode: _focusNode,
-                            controller: _myTextController,
-                            onChanged: onTextChanged,
-                            textCapitalization: TextCapitalization.sentences,
-                            keyboardType: TextInputType.multiline,
-                            textAlign: TextAlign.justify,
-                            maxLines: null,
-                            decoration: InputDecoration(
-                              hintText: 'Yazmaya baslayabilirsin',
-                              hintStyle: TextStyle(color: Colors.grey),
-                              border: InputBorder.none,
-                              focusedBorder: InputBorder.none,
-                              disabledBorder: InputBorder.none,
-                              enabledBorder: InputBorder.none,
-                              errorBorder: InputBorder.none,
-                              focusedErrorBorder: InputBorder.none,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
+              buildTextField(),
+              buildBottomSection()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Expanded buildTextField() {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () => _focusNode.requestFocus(),
+        child: Container(
+          child: Scrollbar(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 4.0),
+                child: TextField(
+                  focusNode: _focusNode,
+                  controller: _myTextController,
+                  onChanged: onTextChanged,
+                  textCapitalization: TextCapitalization.sentences,
+                  keyboardType: TextInputType.multiline,
+                  textAlign: TextAlign.justify,
+                  maxLines: null,
+                  decoration: InputDecoration(
+                    hintText: 'Yazmaya baslayabilirsin',
+                    hintStyle: TextStyle(color: Colors.grey),
+                    border: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                    disabledBorder: InputBorder.none,
+                    enabledBorder: InputBorder.none,
+                    errorBorder: InputBorder.none,
+                    focusedErrorBorder: InputBorder.none,
                   ),
                 ),
               ),
-              buildBottomSection()
-            ],
+            ),
           ),
         ),
       ),
@@ -86,7 +94,7 @@ class _NotePageState extends State<NotePage> {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Text(
-          '${DateHelper().getMMMMd(DateTime.now())} | ${_myTextController.text.length} karakter'),
+          '${DateHelper().getMMMMd(DateTime.now())} | ${_myTextController.text.trim().length} karakter'),
     );
   }
 
@@ -115,7 +123,7 @@ class _NotePageState extends State<NotePage> {
   Expanded buildThemeButton() {
     return Expanded(
       child: IconButton(
-        icon: FaIcon(FontAwesomeIcons.tshirt),
+        icon: Icon(Icons.color_lens),
         onPressed: () {},
       ),
     );
@@ -157,59 +165,95 @@ class _NotePageState extends State<NotePage> {
               color: Theme.of(context).scaffoldBackgroundColor,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  FlatButton.icon(
-                    icon: Icon(Icons.delete),
-                    label: Text("Delete"),
-                    onPressed: () {},
-                  ),
-                  FlatButton.icon(
-                    icon: Icon(Icons.share),
-                    label: Text("Share"),
-                    onPressed: () {},
-                  ),
-                  // FlatButton.icon(
-                  //   icon: Icon(Icons.font_download),
-                  //   label: Text("Change font"),
-                  //   onPressed: () {},
-                  // ),
-                  // FlatButton.icon(
-                  //   icon: Icon(Icons.photo),
-                  //   label: Text("Add photo"),
-                  //   onPressed: () {},
-                  // ),
-                  // FlatButton.icon(
-                  //   icon: Icon(Icons.keyboard_voice),
-                  //   label: Text("Record voice"),
-                  //   onPressed: () {},
-                  // ),
-                  Container(
-                    height: 40,
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      scrollDirection: Axis.horizontal,
-                      itemCount: _myThemes.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Padding(
-                          padding: const EdgeInsets.only(
-                            top: 2.0,
-                            bottom: 8.0,
-                          ),
-                          child: CircleAvatar(
-                            radius: 24,
-                            backgroundColor: _myThemes[index].primaryColor,
-                          ),
-                        );
-                      },
+                  ListTile(
+                    onTap: () {},
+                    leading: Icon(
+                      Icons.delete,
+                      color: Theme.of(context).primaryColor,
                     ),
+                    title: Text("Sil"),
                   ),
+                  ListTile(
+                    onTap: () {},
+                    leading: Icon(
+                      Icons.share,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    title: Text("Paylaş"),
+                  ),
+                  ListTile(
+                    onTap: () {},
+                    leading: Icon(
+                      Icons.font_download,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    title: Text("Yazı tipini değiştir"),
+                  ),
+                  ListTile(
+                    onTap: () {},
+                    leading: Icon(
+                      Icons.photo,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    title: Text("Albümden fotoğraf ekle"),
+                  ),
+                  ListTile(
+                    onTap: () {},
+                    leading: Icon(
+                      Icons.camera_alt,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    title: Text("Kameradan fotoğraf ekle"),
+                  ),
+                  ListTile(
+                    onTap: () {},
+                    leading: Icon(
+                      Icons.keyboard_voice,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    title: Text("Ses kaydı ekle"),
+                  ),
+                  ListTile(
+                    onTap: () {},
+                    leading: Icon(
+                      Icons.voicemail,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    title: Text("Anlık ses kaydet"),
+                  ),
+                  buildColorsContainer(_myThemes),
                 ],
               ),
             ),
           ),
         );
       },
+    );
+  }
+
+  Container buildColorsContainer(List<ThemeData> _myThemes) {
+    return Container(
+      height: 48,
+      child: ListView.builder(
+        shrinkWrap: true,
+        scrollDirection: Axis.horizontal,
+        itemCount: _myThemes.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding: const EdgeInsets.only(
+              top: 2.0,
+              bottom: 8.0,
+            ),
+            child: CircleAvatar(
+              radius: 24,
+              backgroundColor: _myThemes[index].primaryColor,
+            ),
+          );
+        },
+      ),
     );
   }
 
